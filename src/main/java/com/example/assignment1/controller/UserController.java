d100e86828efeba508df4311b00ed9a413e4b768
Naiya Adatiya <113407994+adatiyan@users.noreply.github.com>
2023-04-22 16:49:02 -0400

    Update UserController.java

--- src/main/java/com/example/assignment1/controller/UserController.java
+++ src/main/java/com/example/assignment1/controller/UserController.java
@@ -33,7 +33,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-@RequestMapping("v1/user")
+@RequestMapping("v2/user")
 public class UserController {
 	
 	@Autowired
